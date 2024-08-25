5b522ab25f729d1dbc0534d6a07ba3069c1287b7
Harit tilavat <harittilavat10@gmail.com>
2024-08-26 03:21:37 +0530

    Added Search by country name funcitionality

--- src/app/_services/country/country.service.ts
+++ src/app/_services/country/country.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { CountryModel } from '../../_model';
 import { BaseProviderService } from '../base-provider.service';
 import { HttpClient } from '@angular/common/http';
@@ -9,11 +9,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CountryService extends BaseProviderService {
   private fetchUrl = 'https://restcountries.com/v3.1/all';
+  private fetchUrlByName = 'https://restcountries.com/v3.1/name';
   constructor(http: HttpClient) {
     super(http);
   }
 
   getAllCountry(): Observable<Array<CountryModel>> {
     return this.makeGetCall(this.fetchUrl).pipe(map(res => res));
+  }
+  getCountryByName(name: string): Observable<CountryModel[]> {
+    const url = `${this.fetchUrlByName}/${name}`;
+    return this.makeGetCall(url).pipe(
+      map(res => res),
+      catchError(error => {
+        console.error(`Error fetching country by name (${name}):`, error);
+        return of([]); // Return an empty array in case of error
+      })
+    );
   }
 }
--- src/app/layout/components/home/home.component.ts
+++ src/app/layout/components/home/home.component.ts
@@ -1,50 +1,169 @@
-import { AdService } from './../../../_services/adservice/ad.service';
-import { Component, OnInit, OnDestroy, ElementRef, ViewChild, AfterViewInit, Inject, EventEmitter, Output } from '@angular/core';
+// import { AdService } from './../../../_services/adservice/ad.service';
+// import { Component, OnInit, OnDestroy, ElementRef, ViewChild, AfterViewInit, Inject, EventEmitter, Output } from '@angular/core';
+// import { BrowserService, CountryService } from '../../../_services';
+// import { Router } from '@angular/router';
+// import { FormGroup, FormControl } from '@angular/forms';
+// import { debounceTime, distinctUntilChanged, Subscription, tap } from 'rxjs';
+// import { CountryModel } from '../../../_model';
+
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.scss'
+// })
+// export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+//   public searchForm: FormGroup = new FormGroup({ query: new FormControl('', []) });
+//   public timerInterval!: ReturnType<typeof setInterval>;
+//   public subscription: Array<Subscription> = [];
+//   public isLoading = true;
+//   public countryList: Array<CountryModel> = [];
+//   public displayCountryList: Array<CountryModel> = [];
+//   constructor(public adService: AdService, private browserService: BrowserService, private router: Router, private countryService: CountryService) { }
+
+//   ngOnInit(): void {
+//     if (!this.browserService.isBrowser()) return;
+//     this.subscription.push(this.searchForm.controls['query'].valueChanges.pipe(tap(() => this.isLoading = true), debounceTime(800), distinctUntilChanged())
+//       .subscribe(query => {
+//         if (!query) {
+//           this.displayCountryList = [...this.countryList];
+//           this.isLoading = false;
+//           return;
+//         }
+//         this.fetchCountryByName(query);
+//         console.log("QUERY ", query);
+//       }));
+//     this.fetchAllCountryList();
+//   }
+//   ngAfterViewInit(): void { }
+
+//   fetchAllCountryList(): void {
+//     this.subscription.push(
+//       this.countryService.getAllCountry().subscribe({
+//         next: (res: Array<CountryModel>) => {
+//           console.log("Response : ", res);
+//           this.countryList = res;
+//           this.displayCountryList = [...this.countryList];
+//           this.isLoading = false;
+//         }, error: () => {
+//           console.log("Error in fetching country.");
+//           this.isLoading = false;
+//         }
+//       })
+//     );
+//   }
+//   fetchCountryByName(name: string): void {
+//     this.subscription.push(
+//       this.countryService.getCountryByName(name).subscribe({
+//         next: (res: Array<CountryModel>) => {
+//           console.log("Response : ", res);
+//           this.displayCountryList = res;
+//           this.isLoading = false;
+//         }, error: () => {
+//           console.log("Error in fetching country.");
+//           this.isLoading = false;
+//         }
+//       })
+//     );
+//   }
+//   ngOnDestroy(): void {
+//     this.subscription.forEach(i => i.unsubscribe);
+//   }
+// }
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription, of } from 'rxjs';
+import { tap, catchError, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { AdService } from '../../../_services/adservice/ad.service';
 import { BrowserService, CountryService } from '../../../_services';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, Subscription, tap } from 'rxjs';
 import { CountryModel } from '../../../_model';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy, AfterViewInit, OnDestroy {
-  public searchForm: FormGroup = new FormGroup({ query: new FormControl('', []) });
-  public timerInterval!: ReturnType<typeof setInterval>;
+export class HomeComponent implements OnInit, OnDestroy {
+  public searchForm: FormGroup = new FormGroup({
+    query: new FormControl('')
+  });
   public subscription: Array<Subscription> = [];
-  public isLoading = false;
+  public isLoading = true;
   public countryList: Array<CountryModel> = [];
-  constructor(public adService: AdService, private browserService: BrowserService, private router: Router, private countryService: CountryService) { }
+  public displayCountryList: Array<CountryModel> = [];
+
+  constructor(
+    public adService: AdService,
+    private browserService: BrowserService,
+    private router: Router,
+    private countryService: CountryService
+  ) { }
 
   ngOnInit(): void {
     if (!this.browserService.isBrowser()) return;
-    this.subscription.push(this.searchForm.controls['query'].valueChanges.pipe(tap(() => this.isLoading = true), debounceTime(800), distinctUntilChanged())
-      .subscribe(query => {
+
+    this.subscription.push(this.searchForm.controls['query'].valueChanges.pipe(
+      tap(() => this.isLoading = true),
+      debounceTime(800),
+      distinctUntilChanged(),
+      tap(query => {
+        if (!query) {
+          this.displayCountryList = [...this.countryList];
+          this.isLoading = false;
+          return;
+        }
+        this.fetchCountryByName(query);
+      }),
+      catchError(error => {
+        console.log("Error in search query:", error);
         this.isLoading = false;
-      }));
+        return of([]);
+      })
+    ).subscribe());
+
     this.fetchAllCountryList();
   }
-  ngAfterViewInit(): void { }
 
   fetchAllCountryList(): void {
     this.subscription.push(
-      this.countryService.getAllCountry().subscribe({
-        next: (res: Array<CountryModel>) => {
+      this.countryService.getAllCountry().pipe(
+        tap(() => this.isLoading = true),
+        tap(res => {
           console.log("Response : ", res);
           this.countryList = res;
-        }, error: () => {
-          console.log("Error in fetching country.");
-        }
-      })
+          this.displayCountryList = [...this.countryList];
+        }),
+        tap(() => this.isLoading = false),
+        catchError(error => {
+          console.log("Error in fetching country.", error);
+          this.isLoading = false;
+          return of([]);
+        })
+      ).subscribe()
     );
   }
-  ngOnDestroy(): void {
-    this.subscription.forEach(i => i.unsubscribe);
+
+  fetchCountryByName(name: string): void {
+    this.subscription.push(
+      this.countryService.getCountryByName(name).pipe(
+        tap(() => this.isLoading = true),
+        tap(res => {
+          console.log("Response : ", res);
+          this.displayCountryList = res;
+        }),
+        tap(() => this.isLoading = false),
+        catchError(error => {
+          console.log("Error in fetching country by name.", error);
+          this.isLoading = false;
+          return of([]);
+        })
+      ).subscribe()
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.forEach(sub => sub.unsubscribe());
+  }
 }
-
